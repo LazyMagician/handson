fdc0f35d9ae9af095770119b266da0eccffd3753
Ananthu Achary <ananthu.achary@pelatro.com>
2025-05-22 23:23:32 +0530

    Changed the folder structure

--- DesignPatternModules/ecommercesdk/src/com/ecommercesdk/impls/EcomPaymentSimulationMain.java
+++ DesignPatternModules/ecommercesdk/src/com/ecommercesdk/impls/EcomPaymentSimulationMain.java
@@ -5,9 +5,8 @@ import com.ecommercesdk.interfaces.Gateway;
 import com.ecommercesdk.interfaces.Processor;
 
 public class EcomPaymentSimulationMain {
-
 	public static void main( String[] args ) {
-		Gateway gateway = new PaymentGatewaySdk();
+        Gateway gateway = new PaymentGatewaySdk();
 		Processor processor = new PaymentProcessorAdapter( gateway );
 		Client client = new EcomClient( processor );
 		client.performTransaction( 1000 );
